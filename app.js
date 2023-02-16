910ee34b01ef7f9edfda35bb73ef2252b6c5d4cf
Rafe Uddaraj <78890044+rafeuddaraj@users.noreply.github.com>
2023-02-16 21:05:38 +0600

    text_formated

---
+++ app.js
@@ -0,0 +1,10 @@
+const text = document.getElementById('text_id')
+const shower = document.getElementById('shower')
+addEventListener('keypress', (e) => {
+    if (e.charCode === 13) {
+        col_text = text.value.split(" ").join('_').replace(':','')
+        shower.textContent = col_text
+        text.select()
+        document.execCommand('copy')
+    }
+})
