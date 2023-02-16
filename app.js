b9328f87cb808ab03bd588447ef7e898a36e6ab1
Rafe Uddaraj <78890044+rafeuddaraj@users.noreply.github.com>
2023-02-16 21:15:44 +0600

    fixed problem

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ addEventListener('keypress', (e) => {
     if (e.charCode === 13) {
         col_text = text.value.split(" ").join('_').replace(':','')
         shower.textContent = col_text
+        text.value = col_text
         text.select()
         document.execCommand('copy')
+        // text.value =
     }
 })
